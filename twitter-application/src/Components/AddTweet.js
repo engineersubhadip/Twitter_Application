02edc9fb59eb019595c66d57143a25b3139eb975
Subhadip Das <subhadipdas911@gmail.com>
2024-03-27 21:24:55 +0530

    Passed newly created tweet from Child(Add Tweet) to Parent(Twitter) via Callback Prop

--- twitter-application/src/Components/AddTweet.js
+++ twitter-application/src/Components/AddTweet.js
@@ -1,7 +1,24 @@
-function AddTweet(){
+import {useState} from 'react';
+
+function AddTweet(props){
+
+    let [text,setText] = useState("");
+
     return(
         <>
-            <input placeholder="Please add new tweet..."/>
+            <input 
+            type="text" 
+            value={text} 
+            onChange={(e) => setText(e.target.value)} 
+            placeholder="Please add new tweet..."/>
+
+            <button 
+            onClick={() => {
+                props.updateTodo(text);
+                setText("");
+            }}>
+                Tweet !!
+            </button>
         </>
     )
 }
--- twitter-application/src/Components/Twitter.js
+++ twitter-application/src/Components/Twitter.js
@@ -1,19 +1,22 @@
+import { useState } from "react";
 import AddTweet from "./AddTweet";
 import TweetList from "./TweetList";
 
 function Twitter(){
 
-    let dummyTweets = [
-        {id:12,content:"Tweet Number 1", like:24},
-        {id:20,content:"Tweet Number 2", like:235},
-        {id:24,content:"Tweet Number 3", like:29},
-        {id:26,content:"Tweet Number 4", like:131},
-        {id:29,content:"Tweet Number 5", like:78}
-      ]
+    let [dummyTweets,setDummyTweets] = useState([
+        // {id:12,content:"Tweet Number 1", like:24},
+        // {id:20,content:"Tweet Number 2", like:235},
+        // {id:24,content:"Tweet Number 3", like:29},
+        // {id:26,content:"Tweet Number 4", like:131},
+        // {id:29,content:"Tweet Number 5", like:78}
+      ])
     
     return (
         <div>
-            <AddTweet />
+            <AddTweet 
+            updateTodo=
+            {(newTodo) => setDummyTweets([...dummyTweets,{id:new Date().getTime(), content:newTodo,like:234}])}/>
             <TweetList tweets={dummyTweets}/>
         </div>
     )
